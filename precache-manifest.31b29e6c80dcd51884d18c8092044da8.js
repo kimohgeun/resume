bb93402ad80f21fc469ba2873ed97f6690999fce
kimohgeun <kog1059@gmail.com>
2019-06-16 16:55:58 +0900

    Updates

---
+++ precache-manifest.31b29e6c80dcd51884d18c8092044da8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d4f5bd3a25f61486d171b12079ac38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb958547abdeb153a5c2",
+    "url": "/static/js/2.cd10c7db.chunk.js"
+  },
+  {
+    "revision": "2e3d385afca00e426bd5",
+    "url": "/static/js/main.7d69a136.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c6bab64b183f96aa47077fd3db37afd0",
+    "url": "/static/media/express.c6bab64b.svg"
+  },
+  {
+    "revision": "3e03555fb6ec923e843034f51cdfbf55",
+    "url": "/static/media/hansung.3e03555f.jpg"
+  },
+  {
+    "revision": "44b33af223f010eee87f8c740b348a56",
+    "url": "/static/media/informationProcessingEngineer.44b33af2.png"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "37f66397133477e388655c5edd93b380",
+    "url": "/static/media/ohgeun.37f66397.jpg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "8e96f4f49b5a303c298eb1f7a4e509e8",
+    "url": "/static/media/skons.8e96f4f4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.1f883dbbe489a4804f0c04f4828f9246.js"
+  "/precache-manifest.31b29e6c80dcd51884d18c8092044da8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
